6a02fffb674c7b1a36017bd65d0b142753b333a1
nguyendan25122000 <nguyendan25122000@gmail.com>
2020-10-12 11:35:31 +0700

    sayHi method for dog

---
+++ Dog.js
@@ -0,0 +1,11 @@
+var chalk = require("chalk");
+
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype.sayHi = function() {
+    console.log("Hi! Im " + chalk.red(this.name));
+};
+
+module.exports = Dog;
